f20dbbf08f16dab0b7cba645f09d39c9f34a3279
Roman Zotsenko <romazotsenko15@gmail.com>
2025-04-02 21:30:48 +0200

    Opravená štruktúra ORM, odstránený duplicitný CORS v main.py

--- backend/main.py
+++ backend/main.py
@@ -2,13 +2,9 @@ import fastapi as fa
 from fastapi.middleware.cors import CORSMiddleware
 from router import ALL_ROUTERS
 from router.email import EMAIL_ROUTER
-
-from fastapi.middleware.cors import CORSMiddleware
 from router import authentication
 
-
 API = fa.FastAPI(title="API", version="0.1.0", root_path="/api")
-API.include_router(EMAIL_ROUTER)
 
 API.add_middleware(
     CORSMiddleware,
@@ -18,23 +14,12 @@ API.add_middleware(
     allow_headers=["*"],
 )
 
+API.include_router(EMAIL_ROUTER)
 for router in ALL_ROUTERS:
     API.include_router(router)
+API.include_router(authentication.AUTH_ROUTER)
 
 
-origins = [
-    "http://localhost:5173",
-]
-
-API.add_middleware(
-    CORSMiddleware,
-    allow_origins=["http://localhost:5173"],  # adress frontend
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
 @API.get("/")
 async def root():
     return {"message": "API is working"}
-
-API.include_router(authentication.AUTH_ROUTER)
