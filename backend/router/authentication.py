cb0a72234e7bce0d135ebdbe1a040756f4c3c6ab
Sebastian Kychatyi <kichatyisebastian@gmail.com>
2025-04-06 16:46:32 +0200

    login and password documentation

--- backend/router/authentication.py
+++ backend/router/authentication.py
@@ -26,20 +26,19 @@ class LoginModel(BaseModel): # login model
     user_email: str
     user_password: str
 
-def hash_password(password: str) -> str: #chesovania hesla 
+def hash_password(password: str) -> str: 
     return pwd_context.hash(password)
 
-def verify_password(plain_password: str, hashed_password: str) -> bool: #overovania chesa
+def verify_password(plain_password: str, hashed_password: str) -> bool: 
     return pwd_context.verify(plain_password, hashed_password)
 
 @AUTH_ROUTER.post("/register")
 def register(user: UserModel, db: Session = Depends(get_db)):
-    existing_user = db.query(User).filter_by(user_email=user.user_email).first() #overovania ci je existuje user mail 
+    existing_user = db.query(User).filter_by(user_email=user.user_email).first() 
     if existing_user:
         raise HTTPException(status_code=400, detail="User already exists")
 
     hashed_password = hash_password(user.user_password)
-    #ak nie exsistuje tak sa vytvori novy 
     new_user = User(
         first_name=user.first_name,
         last_name=user.last_name,
@@ -57,11 +56,11 @@ def register(user: UserModel, db: Session = Depends(get_db)):
 
 @AUTH_ROUTER.post("/login")
 def login(user: LoginModel, db: Session = Depends(get_db)):
-    db_user = db.query(User).filter_by(user_email=user.user_email).first() # tu je celkovo overovania na login ci je spravny mail alebo heslo
+    db_user = db.query(User).filter_by(user_email=user.user_email).first() 
     if not db_user or not verify_password(user.user_password, db_user.user_password):
-        raise HTTPException(status_code=400, detail="Invalid email or password") # musi sa pysat ze nepsravne mail alebo heslo, kvoli bezpecnosti nemozem dat ze je zly mail!
+        raise HTTPException(status_code=400, detail="Invalid email or password") 
 
-    token = create_access_token({"sub": db_user.user_email})# tak isto vytvorenia tokena pre usera ktory prisiel do systemu 
+    token = create_access_token({"sub": db_user.user_email})
     return {"access_token": token, "token_type": "bearer"}
 
 @AUTH_ROUTER.get("/me")
--- backend/router/jwt_utils.py
+++ backend/router/jwt_utils.py
@@ -2,17 +2,17 @@ import jwt
 from datetime import datetime, timedelta, timezone
 from fastapi import HTTPException
 
-SECRET_KEY = "Elona_Mask_Pekna_Zena"  #to potom presuniem do env 
+SECRET_KEY = "Trz32_afRs1c_35tf14yvc_hksdpGRRq_2dV"  
 
-#funkcija na vytvarania tokena 
-def create_access_token(data: dict, expires_delta: timedelta = timedelta(hours=12)): #live time tokenu 12 hodin Pan Halvonik povedal ak je tam 24 hodin je to okej preto som dal zatial 12
+
+def create_access_token(data: dict, expires_delta: timedelta = timedelta(hours=12)): 
     to_encode = data.copy()
     expire = datetime.now(timezone.utc) + expires_delta
     to_encode.update({"exp": expire})
     encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm="HS256")
     return encoded_jwt
 
-#funkcija na overovania tokenu 
+
 def verify_access_token(token: str):
     try:
         payload = jwt.decode(token, SECRET_KEY, algorithms=["HS256"])
